bcf2c37afbab38d0b610a6f2ef8562cbd497829d
Vasilis Anagnostopoulos <vasilis.anagnostopoulos.ext@ericsson.com>
2024-02-08 13:43:09 +0200

    Update nvim: - update to harpoon2

--- nvim/lua/modules/plugins/tool.lua
+++ nvim/lua/modules/plugins/tool.lua
@@ -9,6 +9,21 @@ tool["tpope/vim-fugitive"] = {
 -- 	lazy = true,
 -- 	cmd = "LazyGit",
 -- }
+tool["theprimeagen/harpoon"] = {
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("harpoon"):setup()
+    end,
+    keys = {
+      { "<leader>a", function() require("harpoon"):list():append() end, desc = "harpoon file", },
+      { "<C-e>", function() local harpoon = require("harpoon") harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "harpoon quick menu", },
+      { "<C-h>", function() require("harpoon"):list():select(1) end, desc = "harpoon to file 1", },
+      { "<C-t>", function() require("harpoon"):list():select(2) end, desc = "harpoon to file 2", },
+      { "<C-n>", function() require("harpoon"):list():select(3) end, desc = "harpoon to file 3", },
+      { "<C-s>", function() require("harpoon"):list():select(4) end, desc = "harpoon to file 4", },
+    },
+  }
 
 -- Please don't remove which-key.nvim otherwise you need to set timeoutlen=300 at `lua/core/options.lua`
 tool["folke/which-key.nvim"] = {
